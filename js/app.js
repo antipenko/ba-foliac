371c25908e98afe02c957c21de531a578a36e91c
antipenko <sweetglory.victory@gmail.com>
2019-07-18 15:43:46 +0300

    add isotope

--- js/app.js
+++ js/app.js
@@ -42,9 +42,48 @@
           $('.ba-slider').slick('slickGoTo', numberOfSlide);
 	  })
 	  
+	//   add masonry
 	  $('.portfolio-examp').masonry({
 		itemSelector: '.portfolio-item',
 	});
+
+	// add isotope
+	$('.portfolio-examp').isotope();
+
+	var filters = [];
+
+	$('.portfolio-navi').on('click', 'a', function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+
+		var isChecked = $(this).hasClass('active');
+
+		var filter = $(this).attr('data-filter');
+
+		if (isChecked) {
+			addFilter(filter);
+		} else {
+			removeFilter(filter);
+		}
+
+		console.log(filters);
+		$('.portfolio-examp').isotope({
+			filter: filters.join(',')
+		});
+	});
+
+	function addFilter( filter ) {
+	  if ( filters.indexOf( filter ) == -1 ) {
+	    filters.push( filter );
+	  }
+	}
+
+	function removeFilter( filter ) {
+	  var index = filters.indexOf( filter);
+	  if ( index != -1 ) {
+	    filters.splice( index, 1 );
+	  }
+	}
 
 })(jQuery); 
     
