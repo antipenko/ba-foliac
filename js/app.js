8cce4c6c4f55e3fc2e9f48dd58b4ac2d04345277
antipenko <sweetglory.victory@gmail.com>
2019-07-19 00:57:00 +0300

    pampering with a slider and masonry/isotop

--- js/app.js
+++ js/app.js
@@ -45,21 +45,19 @@
 	//   add masonry
 	  $('.portfolio-examp').masonry({
 		itemSelector: '.portfolio-item',
+	
 	});
 
 	// add isotope
-	$('.portfolio-examp').isotope();
+	$('.portfolio-examp').isotope({
+	});
 
 	var filters = [];
-
 	$('.portfolio-navi').on('click', 'a', function(event) {
 		event.preventDefault();
 		$(this).toggleClass('active');
-
 		var isChecked = $(this).hasClass('active');
-
 		var filter = $(this).attr('data-filter');
-
 		if (isChecked) {
 			addFilter(filter);
 		} else {
@@ -71,19 +69,48 @@
 			filter: filters.join(',')
 		});
 	});
-
 	function addFilter( filter ) {
 	  if ( filters.indexOf( filter ) == -1 ) {
-	    filters.push( filter );
+		filters.push( filter );
+		filters.join(',')
 	  }
 	}
-
 	function removeFilter( filter ) {
 	  var index = filters.indexOf( filter);
 	  if ( index != -1 ) {
-	    filters.splice( index, 1 );
+		filters.splice( index, 1 );
+		console.log(filters.join(','));
 	  }
 	}
+
+
+	// new slider
+	$('.ba-slider-new').masonry({
+		itemSelector: '.portfolio-item',
+	});
+
+	function initSlider(){
+		$('.ba-slider-new').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			fade: true,
+			asNavFor: '.ba-slider-new-nav'
+		});
+	}
+
+	$('.ba-slider-new-nav').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.ba-slider-new',
+		dots: false,
+		focusOnSelect: true
+	});
+
+	$('.items-navi button').on('click', function(){
+		initSlider();
+		$('.items-navi button').addClass('nav-btn');
+	})
 
 })(jQuery); 
     
@@ -101,47 +128,3 @@
 
 
 
-
-/*
-
-	$('.portfolio-examp').masonry({
-		itemSelector: '.portfolio-item',
-	});
-
-	$('.portfolio-examp').isotope();
-
-	var filters = [];
-
-	$('.portfolio-navi').on('click', 'a', function(event) {
-		event.preventDefault();
-		$(this).toggleClass('active');
-
-		var isChecked = $(this).hasClass('active');
-
-		var filter = $(this).attr('data-filter');
-
-		if (isChecked) {
-			addFilter(filter);
-		} else {
-			removeFilter(filter);
-		}
-
-		console.log(filters);
-		$('.portfolio-examp').isotope({
-			filter: filters.join(',')
-		});
-	});
-
-	function addFilter( filter ) {
-	  if ( filters.indexOf( filter ) == -1 ) {
-	    filters.push( filter );
-	  }
-	}
-
-	function removeFilter( filter ) {
-	  var index = filters.indexOf( filter);
-	  if ( index != -1 ) {
-	    filters.splice( index, 1 );
-	  }
-	}
-*/
